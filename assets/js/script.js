9c9c923917abfcafcebd856ef53c03c6a731ef5e
Xan <tsadiktalmudim@gmail.com>
2022-02-23 20:18:58 -0600

    corrected errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,12 @@ var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-
+    // check if input values are null
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to complete the task form!");
+        return false;
+    }
+    formE1.reset();
     // package up data as an object
     var taskDataObj = {
         name: taskNameInput,
